36ba1aca63faa36894eb7d0fb5e74261ef32dc5b
Ryan Peden <ryan.peden@gmail.com>
2025-07-27 13:59:09 -0400

    Create ruff config directory, fix .venv script

--- scripts/setup_python_venv.sh
+++ scripts/setup_python_venv.sh
@@ -1,16 +1,16 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
-set -e
+set -ex
 
 VENV_PATH="$HOME/.venv"
-REQS_PATH="$(dirname "$0")/requirements.txt"
+REQS_PATH="$(dirname "$0")/../requirements.txt"
 
 if [ ! -d "$VENV_PATH" ]; then
-  echo "Creating Python virtual environment at $VENV_PATH"
-  python3 -m venv "$VENV_PATH"
+    echo "Creating Python virtual environment at $VENV_PATH"
+    python3 -m venv "$VENV_PATH"
 fi
 
 if [ -f "$REQS_PATH" ]; then
-  echo "Installing Python packages from $REQS_PATH"
-  "$VENV_PATH/bin/pip" install -r "$REQS_PATH"
+    echo "Installing Python packages from $REQS_PATH"
+    "$VENV_PATH/bin/python" -m pip install -r "$REQS_PATH"
 fi
